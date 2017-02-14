3dc8c0a0cbc6f379508a80cee8fd74b7851fe6bf
lu <lubiao.zc@gmail.com>
2017-02-14 18:01:00 +0800

    add admin/orders

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   namespace :admin do
     resources :products
+    resources :orders
   end
 
     resources :products do
@@ -20,7 +21,7 @@ Rails.application.routes.draw do
        post :checkout
     end
   end
-  
+
   resources :orders do
     member do
       post :pay_with_alipay
