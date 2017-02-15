6642a2bf621a45c1e538d0b2b64673bb0dd16e7e
lu <lubiao.zc@gmail.com>
2017-02-15 17:12:23 +0800

    implement welcome#index

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,11 @@
 //= require bootstrap/alert
 //= require bootstrap/dropdown
 //= require_tree .
+
+/*
+	Add to cart fly effect with jQuery. - May 05, 2013
+	(c) 2013 @ElmahdiMahmoud - fikra-masri.by
+	license: http://www.opensource.org/licenses/mit-license.php
+*/
+
+    
