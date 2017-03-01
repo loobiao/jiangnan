82409ce38a78319bef1474804bd848b39ac5552b
lu <lubiao.zc@gmail.com>
2017-03-01 17:06:15 +0800

    add

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170221084328) do
+ActiveRecord::Schema.define(version: 20170228144658) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -68,6 +68,13 @@ ActiveRecord::Schema.define(version: 20170221084328) do
     t.datetime "updated_at",    null: false
   end
 
+  create_table "product_relationships", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "products", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
@@ -77,6 +84,7 @@ ActiveRecord::Schema.define(version: 20170221084328) do
     t.datetime "updated_at",  null: false
     t.text     "image"
     t.string   "category"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
